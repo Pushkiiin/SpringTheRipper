757c511c14637c10eebc6c98045b21aa49babff0
Dmitrii_Prikhozhaev <Dmitrii_Prikhozhaev@epam.com>
2017-12-01 02:37:59 +0300

    color changes

--- src/main/java/screensaver/ColorFrame.java
+++ src/main/java/screensaver/ColorFrame.java
@@ -9,10 +9,8 @@ import java.awt.*;
 import java.util.Random;
 
 @Service
-public class ColorFrame extends JFrame {
-    @Autowired
-    private Color color;
-
+public abstract class ColorFrame extends JFrame {
+    
     public ColorFrame(){
         setSize(200, 200);
         setVisible(true);
@@ -22,7 +20,9 @@ public class ColorFrame extends JFrame {
     public void showOnRandomPlace(){
         Random random = new Random();
         setLocation(random.nextInt(1200), random.nextInt(700));
-        getContentPane().setBackground(color);
+        getContentPane().setBackground(getColor());
         repaint();
     }
+
+    protected abstract Color getColor();
 }
